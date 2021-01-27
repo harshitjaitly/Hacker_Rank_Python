98694c86a609c643564da20fd2208f19c7bf179a
Harshit Jaitly <f20170692@goa.bits-pilani.ac.in>
2021-01-28 04:07:08 +0530

    Create Calendar_Module.py

---
+++ Calendar_Module.py
@@ -0,0 +1,6 @@
+# Link - https://www.hackerrank.com/challenges/calendar-module/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import calendar
+month, day, year = map(int, input().split())
+print((calendar.day_name[calendar.weekday(year, month, day)]).upper())
